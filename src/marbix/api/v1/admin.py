b4dd3e4cd8386133877765f07269a64d8fa06ae8
Rakhim Abdurakhimov <rakhimabdurakhimov@MacBook-Air-Rakhim.local>
2025-07-10 17:08:09 +0500

    added admin role

--- src/marbix/api/v1/admin.py
+++ src/marbix/api/v1/admin.py
@@ -4,7 +4,7 @@ from marbix.schemas.login import AdminLoginRequest, AdminLoginResponse
 from marbix.core.deps import get_db, get_current_admin
 from marbix.services import admin_service
 from marbix.models.user import User
-from marbix.models.make_request import MakeRequest
+from marbix.schemas.strategy import StrategyListItem
 from marbix.schemas.user import UserOut
 from typing import List
 
@@ -27,6 +27,6 @@ def get_user_by_id(user_id: str, admin: User = Depends(get_current_admin), db: S
     return admin_service.get_user_by_id(user_id, db)
 
 
-@router.get("/users/{user_id}/strategies", response_model=List[MakeRequest])
+@router.get("/users/{user_id}/strategies", response_model=List[StrategyListItem])
 def get_user_strategies(user_id: str, admin: User = Depends(get_current_admin), db: Session = Depends(get_db)):
     return admin_service.get_user_strategies(user_id, db)
