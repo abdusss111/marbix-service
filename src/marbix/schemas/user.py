1a84cd3f73e73cb7af0ae26eced52d1a1c8c936d
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-07-11 10:14:34 +0500

    fix: users reg date added

--- src/marbix/schemas/user.py
+++ src/marbix/schemas/user.py
@@ -1,5 +1,4 @@
-# src/marbix/schemas/user.py
-
+from datetime import datetime
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
 class UserOut(BaseModel):
@@ -7,5 +6,5 @@ class UserOut(BaseModel):
     email: str
     name: str
     number: Optional[str] = None
-    created_at: str
+    created_at: datetime
     model_config = ConfigDict(from_attributes=True)
