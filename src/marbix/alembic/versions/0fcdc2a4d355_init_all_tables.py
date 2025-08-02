0b278702f36c712159aef6566a9c1a1d3da3339b
Abdussalam Abdurakhimov <abduss@Abdussalams-MacBook-Air.local>
2025-08-02 11:09:53 +0500

    feat/retry logic to make

--- src/marbix/alembic/versions/0fcdc2a4d355_init_all_tables.py
+++ src/marbix/alembic/versions/0fcdc2a4d355_init_all_tables.py
@@ -1,8 +1,8 @@
-"""add_make_requests_table
+"""init all tables
 
-Revision ID: ffff2eb0fe1d
-Revises: be97a731b159
-Create Date: 2025-07-07 15:19:50.822943
+Revision ID: 0fcdc2a4d355
+Revises: 
+Create Date: 2025-07-28 14:07:51.105643
 
 """
 from typing import Sequence, Union
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'ffff2eb0fe1d'
-down_revision: Union[str, Sequence[str], None] = 'be97a731b159'
+revision: str = '0fcdc2a4d355'
+down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -30,6 +30,7 @@ def upgrade() -> None:
     sa.Column('error', sa.Text(), nullable=True),
     sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
     sa.Column('completed_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('sources', sa.Text(), nullable=True),
     sa.PrimaryKeyConstraint('request_id')
     )
     op.create_index(op.f('ix_make_requests_request_id'), 'make_requests', ['request_id'], unique=False)
@@ -37,6 +38,10 @@ def upgrade() -> None:
     sa.Column('id', sa.String(), nullable=False),
     sa.Column('email', sa.String(), nullable=False),
     sa.Column('name', sa.String(), nullable=True),
+    sa.Column('number', sa.String(), nullable=True),
+    sa.Column('password', sa.String(), nullable=True),
+    sa.Column('role', sa.Enum('ADMIN', 'USER', name='userrole'), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
